a2b46b0f32c6c57346bffe376b8fca358295188e
gBit <gBit@bitMail.com>
2013-02-12 17:54:19 -0600

    Initial Commit

---
+++ PizzaTable/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PizzaTable
+//
+//  Created by gBit on 2/12/13.
+//  Copyright (c) 2013 gBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PizzaTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PizzaTable
+//
+//  Created by gBit on 2/12/13.
+//  Copyright (c) 2013 gBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
